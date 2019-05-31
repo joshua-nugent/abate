4a99121b32df03442065b6988abc151eab4299a2
joshua-nugent <40775762+joshua-nugent@users.noreply.github.com>
2019-05-31 07:10:43 -0400

    Update mock_data_simulator.R
    Vectorized for higher speed.

--- mock_data_simulator.R
+++ mock_data_simulator.R
@@ -5,9 +5,9 @@ library(coxme)
 ##############################################################################
 # NOTE: If you want to quickly test a model fit, reduce the following values
 #       to 20 hospitals and 1000 patients per hospital.
-#       The current settings will take about 6 hours to fit.
+#       The current settings will take 6-8 hours to fit.
 
-hospitals <- 86         # Slightly larger than the number in the moch data set
+hospitals <- 86         # Slightly larger than the number in the mock data set
 patients_per_H <- 5000  # Close to the average in the mock data set
 n_patients <- hospitals * patients_per_H
 ##############################################################################
@@ -19,68 +19,62 @@ beta2 = -.1             # Coefficient for period effect
 beta3 = -.25            # Coefficient for arm*period intervention effect
 
 
-# These values govern the distribution of times to events, time to censor, and overall event rate
+# These values govern the distribution of
+# time to event, time to censor, and overall event rate
 lambdaT = 1350
 lambdaC = lambdaT*3 / 1250
 
 
 ##############################################################################
 simulator <- function(){
-  HospID_mask <- vector()
-  ptid_mask <- vector()
-  linpred <- vector()
-  arm <- vector()
-  period <- vector()
-  pfrail <- vector()
-  hfrail <- vector()
-  time <- vector()
-  eventyn <- vector()
+  patient_vector <- seq(1:n_patients)
   
-  entry <- 0
-  patient <- 0
-  hospital <- 0
+  # Negative binomial distribution chosen
+  # to match distribution of number of visits per patient
+  # in mock data set
+  repeat_visit_vector <- (1 + rnbinom(n = n_patients, size = .7778, mu = .696))
+  entries <- sum(repeat_visit_vector)
+  ptid_mask <- rep(patient_vector, times = repeat_visit_vector)
   
-  for(i in 1:hospitals){
-    a <- ceiling(i %% 2)
-    hospital <- hospital + 1
-
-    # Variance of hospital frailty is .333^2 = .111
-    hospital_frailty <- rnorm(n = 1, mean = 0, sd = .333)
-    for(j in 1:patients_per_H){
-      patient <- patient + 1
-      
-      # Variance of patient frailty is 1.75^2 = 3.06
-      patient_frailty <- rnorm(n = 1, mean = 0, sd = 1.75)
-      
-      # Negative binomial distribution chosen
-      # to match distribution of number of visits per patient
-      # in mock data set
-      for(k in 1:(1+rnbinom(n = 1, size = .7778, mu = .696))){
-        per <- sample(0:1, size = 1)
-        entry <- entry + 1
-        HospID_mask[entry] <- i
-        ptid_mask[entry] <- patient
-        pfrail[entry] <- patient_frailty
-        hfrail[entry] <- hospital_frailty
-        arm[entry] <- a
-        period[entry] <- per
-        linpred <- -beta1*a - beta2*per - beta3*a*per + patient_frailty + hospital_frailty
-        T <- rweibull(n = 1, shape=1, scale=lambdaT*exp(linpred)) 
-        C <- rweibull(n = 1, shape=1, scale=lambdaC)
-        time[entry] <- pmin(T,C)
-        eventyn[entry] <- as.numeric(time[entry]==T)
-        time[entry] <- ceiling(time[entry]) # Whole number time to fit mock data set
-      }
-    }
-  }
-  return(cbind.data.frame(arm, period, HospID_mask, hfrail, ptid_mask, pfrail, linpred, time, eventyn))
+  # Variance of patient frailty is 1.75^2 = 3.06
+  pfrail1 <- rnorm(n = patient_vector, mean = 0, sd = 1.75)
+  pfrail <- rep(pfrail1, times = repeat_visit_vector)
+  
+  HospID_mask1 <- rep(1:hospitals, each = patients_per_H)
+  HospID_mask <- rep(HospID_mask1, times = repeat_visit_vector)
+  
+  # Variance of hospital frailty is .333^2 = .111
+  hfrail1 <- rnorm(n = hospitals, mean = 0, sd = .333)
+  hfrail.1 <- rep(hfrail1, each = patients_per_H)
+  hfrail <- rep(hfrail.1, times = repeat_visit_vector)
+  
+  arm1 <- HospID_mask1 %% 2
+  arm <- rep(arm1, times = repeat_visit_vector)
+  
+  period <- sample(0:1, size = length(arm), replace = TRUE)
+  
+  linpred <- -beta1*arm - beta2*period - beta3*arm*period + pfrail + hfrail
+  
+  event_time <- rweibull(n = entries, shape=1, scale=lambdaT*exp(linpred)) 
+  censor_time <- rweibull(n = entries, shape=1, scale=lambdaC)
+  # Time, continuous
+  cont_time <- pmin(event_time, censor_time) 
+  eventyn <- as.numeric(cont_time==event_time)
+  # Time, as integer, as it is in the mock data set
+  int_time <- ceiling(cont_time) # Time reported contiuously and as an integer in the output
+  
+  dat <- cbind.data.frame(HospID_mask, ptid_mask, linpred, arm, period, pfrail, hfrail, cont_time, int_time, eventyn)
+  colnames(dat) <- c("HospID_mask", "ptid_mask", "linpred", "arm", "period", "pfrail", "hfrail", "cont_time", "int_time", "eventyn")
+  
+  return(dat)
 }
 
 
 ##############################################################################
 # Run the simulator, fit a model, check the results
 test_data <- simulator()
-coxme_model <- coxme(Surv(time = time, eventyn) ~ arm + period + arm*period + (1|HospID_mask/ptid_mask),
-                                    data = test_data)
-summary(coxme_model)    
 
+coxme_model <- coxme(Surv(time = int_time, eventyn) ~ arm + period + arm*period +
+                       (1|HospID_mask/ptid_mask), data = test_data)
+
+summary(coxme_model)
